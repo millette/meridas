dedd73b6b9a1d252429b4b13eace112e99845e48
Robin Millette <robin@millette.info>
2016-04-22 00:06:27 -0400

    Better grouping (lowercase keys).

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,15 @@ const sortBy = require('lodash.sortby')
 const groupBy = require('lodash.groupby')
 const differenceBy = require('lodash.differenceby')
 
+const addKey = (v) => {
+  v._key = (v.html_url || v.repository_url).toLowerCase()
+  return v
+}
+
 // data
-const day1 = require('./day-one.json')
-const day2 = require('./day-two.json')
-const day = differenceBy(day2, day1, (v) => v.platform + (v.html_url || v.repository_url))
+const day1 = require('./day-one.json').map(addKey)
+const day2 = require('./day-two.json').map(addKey)
+const day = differenceBy(day2, day1, (v) => v.platform + v._key)
 
 module.exports = {
   entry: [
@@ -35,7 +40,7 @@ module.exports = {
   jadeLoader: {
     locals: {
       sortBy: sortBy,
-      grouped: groupBy(day, (x) => x.html_url || x.repository_url)
+      grouped: groupBy(day, (x) => x._key)
     }
   },
   postcss: (webpack) => [
