38bd71ecd95664c7555ef25fcb5171ad5da9d894
Robin Millette <robin@millette.info>
2016-04-25 21:24:48 -0400

    Ball of confusion.

--- cron.sh
+++ cron.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-cd dist
+cd /home/millette/meridas/dist/
 yesterday=`date --date=yesterday +"%Y-%m-%d"`
 mv index.html $yesterday.html
-cd -
 
-mkdir working
-cd working
+cd ../working
 mv day-two.json day-one.json
+. /home/millette/.exports
 ../node_modules/.bin/star-where millette -n 50 > day-two.json
-cd -
 
+cd ..
 npm run make
 export SSH_AUTH_SOCK=$( ls /tmp/ssh-*/agent* )
 rsync -vaPS dist/* rollodeqc.com:/var/local/www/meridas.rollodeqc.com/public/millette/
